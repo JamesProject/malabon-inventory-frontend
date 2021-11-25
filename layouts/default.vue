852da0f9c08b3f567e63cae079124af804bb6003
jamesulip <ulip.james@gmail.com>
2021-11-26 04:01:07 +0800

    sample template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,29 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <v-navigation-drawer :mini-variant="miniVariant" :clipped="clipped" fixed app>
+
+      <v-list-item>
+        <div class="px-2">
+         <v-avatar>
+          <img
+            src="https://cdn.vuetifyjs.com/images/john.jpg"
+            alt="John"
+          >
+        </v-avatar>
+        </div>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            James Ulip
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Programmer
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
       <v-list>
-        <v-list-item
-          no-action
-          sub-group
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item no-action sub-group v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -24,66 +32,56 @@
           </v-list-item-content>
         </v-list-item>
 
-        <v-list-group
-        :value="true"
-        prepend-icon="mdi-account-circle"
-      >
-        <template v-slot:activator>
-          <v-list-item-title>Users</v-list-item-title>
-        </template>
-
-        <v-list-group
-
-          no-action
-          sub-group
-        >
+        <v-list-group :value="true" prepend-icon="mdi-account-circle">
           <template v-slot:activator>
-            <v-list-item-content>
-              <v-list-item-title>Admin</v-list-item-title>
-            </v-list-item-content>
+            <v-list-item-title>Users</v-list-item-title>
           </template>
 
-          <v-list-item
-            v-for="([link], i) in admins"
-            :key="i"
-            router
-            :to="link.to"
-          >
-            <v-list-item-title v-text="link.title"></v-list-item-title>
-            <v-icon>{{link.icon}}</v-icon>
-            <v-list-item-icon>
-              <v-icon v-text="link.icon"></v-icon>
-            </v-list-item-icon>
-          </v-list-item>
+          <v-list-group no-action sub-group>
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>Admin</v-list-item-title>
+              </v-list-item-content>
+            </template>
+
+            <v-list-item v-for="([link], i) in admins" :key="i" router :to="link.to">
+              <v-list-item-title v-text="link.title"></v-list-item-title>
+              <v-icon>{{link.icon}}</v-icon>
+              <v-list-item-icon>
+                <v-icon v-text="link.icon"></v-icon>
+              </v-list-item-icon>
+            </v-list-item>
+          </v-list-group>
+
+
         </v-list-group>
-
-
-      </v-list-group>
       </v-list>
+
+      <template v-slot:append>
+        <v-list>
+          <v-list-item no-action sub-group>
+            <v-list-item-action>
+              <v-icon>mdi-logout</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="`Logout`" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </template>
+
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="miniVariant = !miniVariant" />
-      <v-menu
-        left
-        bottom
-      >
+      <v-menu left bottom>
         <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
+          <v-btn icon v-bind="attrs" v-on="on">
             <v-icon>mdi-dots-vertical</v-icon>
           </v-btn>
         </template>
 
         <v-list>
-          <v-list-item
-          >
+          <v-list-item>
             <v-list-item-title>Admin Page</v-list-item-title>
           </v-list-item>
         </v-list>
@@ -101,51 +99,50 @@
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      clipped: true,
-      drawer: false,
-      fixed: false,
-      admins: [
-        [
-          {
-            title: 'Settings',
-            to: '/settings'
-          },
-          {
-            title: 'Permission',
-            to: '/'
-          },
-           {
-            title: 'Roles',
-            to: '/'
-          },
+  export default {
+    data() {
+      return {
+        clipped: true,
+        drawer: false,
+        fixed: false,
+        admins: [
+          [{
+              title: 'Settings',
+              to: '/settings'
+            },
+            {
+              title: 'Permission',
+              to: '/'
+            },
+            {
+              title: 'Roles',
+              to: '/'
+            },
+          ],
         ],
-      ],
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-clipboard-edit-outline',
-          title: 'Invenrory',
-          to: '/inspire'
-        },
-        {
-          icon: 'cart',
-          title: 'Categories',
-          to: '/categories'
-        },
+        items: [{
+            icon: 'mdi-apps',
+            title: 'Welcome',
+            to: '/'
+          },
+          {
+            icon: 'mdi-clipboard-edit-outline',
+            title: 'Inventory',
+            to: '/inspire'
+          },
+          {
+            icon: 'mdi-format-list-checks',
+            title: 'Categories',
+            to: '/categories'
+          },
 
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Inventory'
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Inventory'
+      }
     }
   }
-}
+
 </script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,20 +1,10 @@
 <template>
-<div>
-   <!-- Confirm delete dialog -->
-  <div class="w-full" >
-    <v-dialog
-        v-model="deleteDialog"
-        persistent
-        max-width="290"
-      >
+  <div>
+    <!-- Confirm delete dialog -->
+    <div class="w-full">
+      <v-dialog v-model="deleteDialog" persistent max-width="290">
         <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            color="primary"
-            dark
-            v-bind="attrs"
-            v-on="on"
-            right
-          >
+          <v-btn color="primary" dark v-bind="attrs" v-on="on" right>
             Add User
           </v-btn>
         </template>
@@ -22,101 +12,269 @@
           <v-card-title class="text-h5">
             Use Google's location service?
           </v-card-title>
-          <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+          <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google,
+            even when no apps are running.</v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn
-              color="green darken-1"
-              text
-              @click="dialog = false"
-            >
+            <v-btn color="green darken-1" text @click="dialog = false">
               Disagree
             </v-btn>
-            <v-btn
-              color="green darken-1"
-              text
-              @click="dialog = false"
-            >
+            <v-btn color="green darken-1" text @click="dialog = false">
               Agree
             </v-btn>
           </v-card-actions>
         </v-card>
-    </v-dialog>
+      </v-dialog>
+    </div>
+    <v-data-table v-model="selected" :headers="headers" :items="desserts" :single-select="singleSelect" item-key="name"
+      show-select class="elevation-1">
+      <template v-slot:item.active="{ item }">
+         <v-checkbox
+          v-model="item.active"
+        ></v-checkbox>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon small class="mr-2" @click="editItem(item)">
+          mdi-pencil
+        </v-icon>
+        <v-icon small @click="deleteItem(item)">
+          mdi-delete
+        </v-icon>
+      </template>
+    </v-data-table>
+
+
+
   </div>
-  <v-data-table
-    v-model="selected"
-    :headers="headers"
-    :items="desserts"
-    :single-select="singleSelect"
-    item-key="name"
-    show-select
-    class="elevation-1"
-  >
-    <template v-slot:item.actions="{ item }">
-      <v-icon
-        small
-        class="mr-2"
-        @click="editItem(item)"
-      >
-        mdi-pencil
-      </v-icon>
-      <v-icon
-        small
-        @click="deleteItem(item)"
-      >
-        mdi-delete
-      </v-icon>
-    </template>
-  </v-data-table>
-
-
-
-</div>
 </template>
 <script>
   export default {
-    data () {
+    data() {
       return {
         singleSelect: false,
         selected: [],
         deleteDialog: false,
-        headers: [
-          {
+        headers: [{
             text: 'Full Name',
             align: 'start',
             sortable: true,
             value: 'name',
           },
-          { text: 'Active', value: 'active' },
-          { text: 'Roles', value: 'roles' },
-          { text: 'Created at', value: 'created_at' },
-          { text: 'Updated at', value: 'updated_at' },
-          { text: 'Actions', value: 'actions' },
-        ],
-        desserts: [
           {
-            name: 'James Ulip',
-            active: '159',
-            role: [1,2,3,4],
-            created_at: 24,
-            updated_at: 4.0,
-            actions: false,
+            text: 'Active',
+            value: 'active'
           },
           {
-            name: 'John Doe',
-            active: '159',
-            role: 6.0,
-            created_at: 24,
-            updated_at: 4.0,
-            actions: false,
+            text: 'Roles',
+            value: 'roles'
+          },
+          {
+            text: 'Created at',
+            value: 'created_at'
+          },
+          {
+            text: 'Updated at',
+            value: 'updated_at'
+          },
+          {
+            text: 'Actions',
+            value: 'actions'
           },
         ],
+        desserts: [{
+          "name": "Alameda Sweedy",
+          "active": false,
+          "role": [{},
+            {}
+          ],
+          "created_at": "3/9/2021",
+          "updated_at": "10/31/2021",
+          "actions": false
+        }, {
+          "name": "Yardley Rouge",
+          "active": true,
+          "role": [{},
+            {},
+            {},
+            {}
+          ],
+          "created_at": "10/1/2021",
+          "updated_at": "10/20/2021",
+          "actions": false
+        }, {
+          "name": "Angus Woolforde",
+          "active": true,
+          "role": [{},
+            {},
+            {}
+          ],
+          "created_at": "3/23/2021",
+          "updated_at": "2/9/2021",
+          "actions": true
+        }, {
+          "name": "Grier Dorsett",
+          "active": false,
+          "role": [{},
+            {},
+            {},
+            {},
+            {}
+          ],
+          "created_at": "3/19/2021",
+          "updated_at": "7/19/2021",
+          "actions": false
+        }, {
+          "name": "Gunther Adnams",
+          "active": true,
+          "role": [{},
+            {},
+            {},
+            {},
+            {}
+          ],
+          "created_at": "4/29/2021",
+          "updated_at": "9/24/2021",
+          "actions": false
+        }, {
+          "name": "Lindi Undrell",
+          "active": false,
+          "role": [{},
+            {},
+            {},
+            {}
+          ],
+          "created_at": "3/16/2021",
+          "updated_at": "8/15/2021",
+          "actions": true
+        }, {
+          "name": "Sharity Pyke",
+          "active": true,
+          "role": [{},
+            {},
+            {},
+            {},
+            {}
+          ],
+          "created_at": "10/3/2021",
+          "updated_at": "11/28/2020",
+          "actions": true
+        }, {
+          "name": "Ethelyn Greenhouse",
+          "active": true,
+          "role": [{}],
+          "created_at": "4/22/2021",
+          "updated_at": "12/6/2020",
+          "actions": false
+        }, {
+          "name": "Jonah Rickerby",
+          "active": true,
+          "role": [
+
+          ],
+          "created_at": "5/22/2021",
+          "updated_at": "1/13/2021",
+          "actions": true
+        }, {
+          "name": "Ham Radbourn",
+          "active": false,
+          "role": [{},
+            {}
+          ],
+          "created_at": "8/14/2021",
+          "updated_at": "12/23/2020",
+          "actions": false
+        }, {
+          "name": "Harmonia Malyan",
+          "active": true,
+          "role": [
+
+          ],
+          "created_at": "2/16/2021",
+          "updated_at": "2/26/2021",
+          "actions": true
+        }, {
+          "name": "Kassia Montague",
+          "active": false,
+          "role": [{}],
+          "created_at": "3/17/2021",
+          "updated_at": "11/23/2021",
+          "actions": true
+        }, {
+          "name": "Gilly Ramelot",
+          "active": false,
+          "role": [{},
+            {},
+            {}
+          ],
+          "created_at": "11/18/2021",
+          "updated_at": "10/1/2021",
+          "actions": false
+        }, {
+          "name": "Tilda Segar",
+          "active": true,
+          "role": [{}],
+          "created_at": "10/27/2021",
+          "updated_at": "3/13/2021",
+          "actions": true
+        }, {
+          "name": "Bev Popley",
+          "active": false,
+          "role": [{}],
+          "created_at": "9/17/2021",
+          "updated_at": "9/29/2021",
+          "actions": false
+        }, {
+          "name": "Brigit Lumm",
+          "active": true,
+          "role": [{},
+            {},
+            {},
+            {}
+          ],
+          "created_at": "5/1/2021",
+          "updated_at": "2/25/2021",
+          "actions": false
+        }, {
+          "name": "Nikos Casel",
+          "active": true,
+          "role": [
+
+          ],
+          "created_at": "11/12/2021",
+          "updated_at": "6/13/2021",
+          "actions": true
+        }, {
+          "name": "Brandy Penquet",
+          "active": false,
+          "role": [{},
+            {},
+            {}
+          ],
+          "created_at": "7/15/2021",
+          "updated_at": "9/14/2021",
+          "actions": true
+        }, {
+          "name": "Tamara Trickett",
+          "active": false,
+          "role": [{}],
+          "created_at": "5/25/2021",
+          "updated_at": "2/13/2021",
+          "actions": true
+        }, {
+          "name": "Bourke Bover",
+          "active": true,
+          "role": [{}],
+          "created_at": "12/22/2020",
+          "updated_at": "5/8/2021",
+          "actions": true
+        }],
       }
     },
-    methods:{
-      editItem(){
-        this.deleteDialog  = true
+    methods: {
+      editItem() {
+        this.deleteDialog = true
       }
     }
   }
+
 </script>
